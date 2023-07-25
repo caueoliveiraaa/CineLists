4199c9edeb09eb70eedab3ed7b761a6d0467d71e
natancordeiro <leo.siscobra@gmail.com>
2023-07-25 00:07:17 -0300

    Creating Flask app

--- python_website/app.py
+++ python_website/app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask 
+from views import views
+
 
 # Create app
 app = Flask(__name__)
-
-
-
-
-
+# Register the blueprint views
+app.register_blueprint(views, url_prefix='/')
 
 
 if __name__== '__main__':
+    # Run app in port 8001
     app.run(debug=True, port=8001)
--- python_website/views.py
+++ python_website/views.py
@@ -1,1 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+
+
+# Create a bluprint 
+views = Blueprint(__name__, 'views')
+
+# Create route
+@views.route('/')
+def home():
+    # Send variables to template and render it 
+    return render_template('index.html', name='Robot')
+
+
+# Create another route for user names getting the user name after /
+# @views.route('/profile/<username>')
+# def profile(username):
+#     args = request.args
+#     name = args.get('name')
+#     return render_template('index.html', name=username)
+
+
+# Get username with ?name=user_name_here
+@views.route('/profile/')
+def profile():
+    args = request.args
+    name = args.get('name')
+    return render_template('index.html', name=name)
+ 
