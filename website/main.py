168c5a3ca385d6970c455f615db57e8e6165240d
caueoliveiraaa <leo.siscobra@gmail.com>
2024-07-07 20:41:59 -0300

    First page - Movies
    (Cauê Oliveira 07/07/2024) feat: Including the first movie page display all the movies.

--- website/main.py
+++ website/main.py
@@ -5,17 +5,36 @@ This website displays and orgnizes movies and series.
 
 
 import uvicorn
+import json
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from pathlib import Path
 
 app = FastAPI(title='Cine List', description='CRUD for movies')
+templates = Jinja2Templates(directory="templates") 
   
+  
+def load_movies() -> dict:
+    json_file_path = Path(__file__).parent / 'database' / 'movies.json'
+    
+    with open(json_file_path) as file:
+        return json.load(file)
+
 
 @app.get('/')
 def root() -> dict:
     return { "status": 200 }
 
-    
+
+@app.get('/movies', response_class=HTMLResponse)
+def get_movies(request: Request):
+    movies = load_movies()
+    return templates.TemplateResponse("movies.html", {"request": request, "movies": movies})
+
+
 def main() -> None:
     try:
         uvicorn.run(app, host='0.0.0.0', port=8000)
